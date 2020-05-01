16c6a3f06ee37a82c23aa552db31e8797a32bb4b
s-rajput <64627796+s-rajput@users.noreply.github.com>
2020-05-01 17:05:57 +1000

    Update fun-data.spec.ts

--- tests/unit/api/fun-data.spec.ts
+++ tests/unit/api/fun-data.spec.ts
@@ -2,7 +2,7 @@ import * as c from '@/api/fun-data'
 
 describe('currency model', () => {
     describe('convert function', () => {
-        it('should convert fare to foreign fare based on the bsr if a fare is passed in', () => {
+        it('should be able to authenticate', () => {
             const output = c.authenticate('test','tt','')
             expect(output.then(function(result) { })).toEqual(140)
         })
